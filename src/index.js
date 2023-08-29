2bbd1b090caaf3b5bb1769b44e9cf594781ebde9
megukentarou <61212485+megukentarou@users.noreply.github.com>
2023-08-29 22:50:02 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// /*
+//  * const,let等の変数宣言
+//  */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+
+/**
+ * 分割代入
+ */
